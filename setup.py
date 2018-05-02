9015d4484be74b911f25e0f3c00b0395284ebad6
Steven Herbst <sherbst@stanford.edu>
2018-05-02 15:13:10 -0700

    python code package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flystim",
+    version="0.0.1",
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
