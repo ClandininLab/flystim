a9c3f5f11a38a699bee9ef1bef6ddacfe7518bde
Max Turner <maxwellholteturner@gmail.com>
2019-11-26 13:49:57 -0800

    Update rotating_grating.py

--- examples/rotating_grating.py
+++ examples/rotating_grating.py
@@ -10,25 +10,34 @@ from time import sleep
 
 
 def main():
-    # Define screen(s) for the rig
-    w = 20.6e-2; h = 12.8e-2; # meters of image at projection plane
-
     def_screen = Screen(fullscreen=False, server_number = 1, id = 0, vsync=True)
-    aux_screen = Screen(width=w, height=h, rotation=pi-pi/4, offset=(4.0e-2, 3.9e-2, -6.1e-2), id=0, fullscreen=False, vsync=True, square_side=0)
 
-    # draw_screens([aux_screen, def_screen])
-    # plt.show()
+    #AODscope screen:
+    pts = [
+            ((1.0, 1.0), (20.1e-3, -46.3e-3, 12.0e-3)),
+            ((-0.70, 1.0), (20.1e-3, +72.4e-3, 12.0e-3)),
+            ((-0.70, -1.0), (20.1e-3, +72.4e-3, -82.9e-3)),
+            ((1.0, -1.0), (20.1e-3, -46.3e-3, -82.9e-3))
+        ]
 
-    manager = launch_stim_server(aux_screen)
+    tri_list = Screen.quad_to_tri_list(*pts)
+    AODscope_screen = Screen(tri_list=tri_list, server_number=1, id=0, fullscreen=False, vsync=True, square_side=0, square_loc='ll')
+
+
+    draw_screens([def_screen, AODscope_screen])
+    plt.show()
+
+# # # # # # # # # # # # # # # # # # # # #
+    manager = launch_stim_server(AODscope_screen)
 
 
     tv_pairs = [(0, 0), (4, 360)]
     angle_traj = Trajectory(tv_pairs, kind='linear').to_dict()
 
     manager.load_stim(name='RotatingGrating', rate=25, period=25, mean=0.5, contrast=1.0, offset=0.0, profile='square',
-                      color=[1, 1, 1, 1], cylinder_radius=1.1, cylinder_height=10, theta=180, phi=0, angle=0)
+                      color=[1, 1, 1, 1], cylinder_radius=1.1, cylinder_height=10, theta=0, phi=0, angle=0)
 
-    tv_pairs = [(0, 180-45), (4, 180+135)]
+    tv_pairs = [(0, 0), (4, 360)]
     theta_traj = Trajectory(tv_pairs, kind='linear').to_dict()
 
     tv_pairs = [(0, 0), (4, 360)]
@@ -37,9 +46,9 @@ def main():
     tv_pairs = [(0, 45), (4, -45)]
     phi_traj = Trajectory(tv_pairs, kind='linear').to_dict()
 
-    manager.load_stim(name='MovingPatch',width=5, height=5, phi=-30, color=0.5, theta=225, hold=True, angle=90)
+    manager.load_stim(name='MovingPatch',width=10, height=10, phi=0, color=0.5, theta=0, hold=True, angle=0)
 
-    # manager.load_stim(name='Tower', color=[1, 0, 0, 1], cylinder_location=[0, 1, -0.5],  cylinder_height=3, cylinder_radius=2, hold=True) #red +y
+    manager.load_stim(name='Tower', color=[1, 0, 0, 1], cylinder_location=[1, 0, 0],  cylinder_height=1, cylinder_radius=0.5, hold=True) #red +y
 
     sleep(1)
 
