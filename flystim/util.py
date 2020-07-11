2e8e51efa46c1d7bcd61edb70a0b59a7556bc811
Andrew Berger <andbberger@gmail.com>
2020-07-11 06:46:09 -0700

    add local lag analysis to latency_report

--- flystim/util.py
+++ flystim/util.py
@@ -1,6 +1,8 @@
 from math import sin, cos
 from numbers import Number
+
 import numpy as np
+from scipy.interpolate import interp1d
 
 def listify(x, type_):
     if isinstance(x, (list, tuple)):
@@ -89,24 +91,24 @@ def get_rgba(val, def_alpha=1):
     else:
         raise ValueError(f'Cannot use value with length {len(val)}.')
 
-# TODO: precisely when are flystim measurements taken? right before the frame is rendered?
-def latency_report(flystim_timestamps, flystim_sync, fictrac_timestamps, fictrac_sync):
+def latency_report(flystim_timestamps, flystim_sync, fictrac_timestamps, fictrac_sync,
+                   window_size=10, n_windows=32):
     """ Latency analysis report
 
     NOTE: this method was written to analyze timestamp logs as recorded. In particular, flystim_timestamps has units of
     seconds but fictrac_timestamps has units of milliseconds!!
 
     Args:
-      flystim_timestamps: list of timestamps when sync square was updated - (n_fs, )
+      flystim_timestamps: list of timestamps when sync square was updated - (n_fs,)
         units: seconds
-      flystim_sync: list of sync square states, as recorded by flystim - (n_fs, )
-      fictrac_timestamps: list of timestamps when fictrac captured a frame - (n_ft, )
+      flystim_sync: list of sync square states, as recorded by flystim - (n_fs,)
+      fictrac_timestamps: list of timestamps when fictrac captured a frame - (n_ft,)
         units: milliseconds
-      fictrac_sync: list of sync square states, as captured by fictrac - (n_ft, )
+      fictrac_sync: list of sync square states, as captured by fictrac - (n_ft,)
+      window_size: size of window to use for local latency analysis
+      n_windows: number of windows to compute lag for
 
     """
-    from scipy.interpolate import interp1d
-
     assert len(flystim_timestamps) == len(flystim_sync)
     assert len(fictrac_timestamps) == len(fictrac_sync)
 
@@ -183,19 +185,38 @@ def latency_report(flystim_timestamps, flystim_sync, fictrac_timestamps, fictrac
 
     resample_frame_len = np.mean(np.diff(fictrac_timestamps))
 
-    time_grid = np.arange(
+    time_bounds = (
         max(min(flystim_timestamps), min(fictrac_timestamps)),
         min(max(flystim_timestamps), max(fictrac_timestamps)),
-        resample_frame_len
     )
+
+    time_grid = np.arange(*time_bounds, resample_frame_len)
 
     resampled_fs_sync = flystim_interp(time_grid)
     resampled_ft_sync = fictrac_interp(time_grid)
 
     global_lag = calculate_lag(resampled_fs_sync, resampled_ft_sync)
 
-    print("Globally optimal lag: {:.1f} ms".format(global_lag * resample_frame_len * 1000))
+    print("Globally optimal lag: {:.1f}ms".format(global_lag * resample_frame_len * 1000))
 
+    local_lags = []
+
+    for start_time in np.linspace(time_bounds[0], time_bounds[1] - window_size, n_windows):
+        time_grid = np.arange(start_time, start_time + window_size, resample_frame_len)
+
+        local_lags.append(
+            calculate_lag(
+                flystim_interp(time_grid),
+                fictrac_interp(time_grid)
+            ) * resample_frame_len * 1000
+        )
+
+
+    print("Local lag ({} {}s windows): {:.1f}ms mean, {:.1f}ms std".format(
+        n_windows, window_size, np.mean(local_lags), np.std(local_lags)
+    ))
+
+    print("Total length of recording: {:1f} s".format(time_bounds[1] - time_bounds[0]))
 
 # TODO: test!
 # TODO: mean zero sequences?
