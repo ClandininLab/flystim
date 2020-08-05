db1fab7dcd45c09ff0eeb57db35ecec94e124ff8
Max <maxwellholteturner@gmail.com>
2020-08-04 17:01:46 -0700

    Remove obsolete files

--- flystim/__init__.py
+++ flystim/__init__.py
@@ -1,4 +1,2 @@
-from .files import rel_path
 from .util import normalize, rotx, roty, rotz, get_rgba, translate, scale
-from .cave import CaveSystem
 from .shapes import GlQuad, GlTri, GlVertices, GlCube, GlSphericalRect, GlSphericalCirc, GlCylinder, GlSphericalPoints
--- flystim/files.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-def top_dir():
-    return Path(__file__).resolve().parent.parent
-
-def rel_path(*args):
-    return Path(top_dir(), *args)
