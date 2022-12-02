0c4e7de9e83e1bbb562b5832de8289176a192e62
Magneto <baccuslab@magneto.stanford.edu>
2022-12-02 03:23:09 -0800

    after jbm029

--- experiment.py
+++ experiment.py
@@ -17,121 +17,254 @@ from flystim.experiments import init_screens, get_video_dim
 from time import sleep
 
 def main():
-    baker_path = '/home/baccuslab/Videos/stimulus_videos/TEST_BAKER.avi'
-    nyc_path = '/home/baccuslab/Videos/stimulus_videos/TEST_CITY.avi'
+    INTERVAL=1
+
+    N_TEST=10
+    N_TRAIN=1
+
+    TRAIN_DUR=20*60
+    TEST_DUR = 20
+
+    WN_NPIX = 200
+
+
+    test_baker_path = '/home/baccuslab/Videos/stimulus_videos/TEST_BAKER.avi'
+    test_nyc_path = '/home/baccuslab/Videos/stimulus_videos/TEST_CITY.avi'
+    train_baker_path = '/home/baccuslab/Videos/stimulus_videos/TRAIN_BAKER.avi'
+    train_nyc_path = '/home/baccuslab/Videos/stimulus_videos/TRAIN_CITY.avi'
     manager = init_screens()
     
     manager.black_corner_square()
     manager.set_idle_background(0)
-    manager.set_global_fly_pos(0,0,-0.3)
     manager.start_stim()
     manager()
-    n_pix=200
-    # for i in range(3):
-    #     ### INIT 
-    #     # break
-    #     manager.black_corner_square()
-    #     manager.set_idle_background(0)
-    #     manager()
-
-    #     sleep(3)
-        
-    #     ### WHITE NOISE
-    #     rwg = random_word.RandomWords()
-        
-    #     memname = rwg.get_random_word()
-    #     wn = WhiteNoise(memname, (n_pix,int(800/1920*n_pix)), 20)
-    #     p = threading.Thread(target=wn.stream).start()
-    #     manager.start_corner_square()
-    #     manager.load_stim(name='PixMap', memname=memname, frame_size=(int(800/1920*n_pix),n_pix,3))
-    #     manager.start_stim()
-    #     manager()
-
-    #     sleep(3)
-        
-    #     manager.stop_stim()
-    #     manager.black_corner_square()
-    #     manager.set_idle_background(0)
-    #     manager()
-
-    #     sleep(2)
-
-    #     del wn,p
     
-    # for i in range(3):
-    #     ### NATURAL SCENE
-    #     rwg = random_word.RandomWords()
-    #     memname = rwg.get_random_word()
-    #     dim = get_video_dim(baker_path)
-    #     nm = NaturalMovie(memname, baker_path, 60)
-    #     nm.warmup(20)
-    #     p = threading.Thread(target=nm.stream).start()
-    #     sleep(2)
-    #     manager.start_corner_square()
-    #     manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
-    #     manager.start_stim()
-    #     manager()
-    #     sleep(3)    #     manager.set_global_fly_pos(0,0,0)
-    #     manager.stop_stim()
-    #     manager.set_idle_background(0)
-    #     manager.black_corner_square()
-    #     manager()
-    #     sleep(2)
-    #     nm.saveout()
-    #     del nm,p
-            
-    for i in range(5):
+    ##### TEST ####
+    manager.set_global_fly_pos(0,0,-0.5)
+    # WN
+    for i in range(N_TEST):
+        manager.black_corner_square()
+        manager.set_idle_background(0)
+        manager()
+
+        sleep(INTERVAL)
+        
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        wn = WhiteNoise(memname, (WN_NPIX,int(800/1920*WN_NPIX)), 20, seed=37)
+        p = threading.Thread(target=wn.stream).start()
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=(int(800/1920*WN_NPIX),WN_NPIX,3))
+        manager.start_stim()
+        manager()
+
+        sleep(TEST_DUR)
+        
+        manager.stop_stim()
+        manager.black_corner_square()
+        manager.set_idle_background(0)
+        manager()
+
+        sleep(INTERVAL)
+
+        del wn,p
+    
+    # BAKER
+    for i in range(N_TEST):
         ### NATURAL SCENE
         rwg = random_word.RandomWords()
         memname = rwg.get_random_word()
-        dim = get_video_dim(nyc_path)
-        nm = NaturalMovie(memname, nyc_path, 60)
-        nm.warmup(20)
+        dim = get_video_dim(test_baker_path)
+        nm = NaturalMovie(memname, test_baker_path, 60)
         p = threading.Thread(target=nm.stream).start()
-        sleep(2)
+        sleep(INTERVAL)
         manager.start_corner_square()
         manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
         manager.start_stim()
         manager()
-        sleep(30)    #     manager.set_global_fly_pos(0,0,0)
+        sleep(TEST_DUR)    #     manager.set_global_fly_pos(0,0,0)
+        manager.stop_stim()
+        manager.set_idle_background(0)
+        manager.black_corner_square()
+        manager()
+        sleep(INTERVAL)
+        nm.saveout()
+        del nm,p
+
+    # NYC  
+    for i in range(N_TEST):
+        ### NATURAL SCENE
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        dim = get_video_dim(test_nyc_path)
+        nm = NaturalMovie(memname, test_nyc_path, 60)
+        nm.warmup(20)
+        p = threading.Thread(target=nm.stream).start()
+        sleep(INTERVAL)
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
+        manager.start_stim()
+        manager()
+        sleep(TEST_DUR)    #     manager.set_global_fly_pos(0,0,0)
         manager.stop_stim()
 
         manager.black_corner_square()
         manager()
-        sleep(2)
+        sleep(INTERVAL)
+        nm.saveout()
+        del nm,p
+
+    ##### TRAIN
+    
+    # WN
+    for i in range(N_TRAIN):
+        manager.black_corner_square()
+        manager.set_idle_background(0)
+        manager()
+
+        sleep(INTERVAL)
+        
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        wn = WhiteNoise(memname, (WN_NPIX,int(800/1920*WN_NPIX)), 20, seed=17)
+        p = threading.Thread(target=wn.stream).start()
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=(int(800/1920*WN_NPIX),WN_NPIX,3))
+        manager.start_stim()
+        manager()
+
+        sleep(TRAIN_DUR)
+        
+        manager.stop_stim()
+        manager.black_corner_square()
+        manager.set_idle_background(0)
+        manager()
+
+        sleep(INTERVAL)
+
+        del wn,p
+    # BAKER 
+    for i in range(N_TRAIN):
+        ### NATURAL SCENE
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        dim = get_video_dim(train_baker_path)
+        nm = NaturalMovie(memname, train_baker_path, 60)
+        p = threading.Thread(target=nm.stream).start()
+        sleep(INTERVAL)
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
+        manager.start_stim()
+        manager()
+        sleep(TRAIN_DUR)    #     manager.set_global_fly_pos(0,0,0)
+        manager.stop_stim()
+        manager.set_idle_background(0)
+        manager.black_corner_square()
+        manager()
+        sleep(INTERVAL)
+        nm.saveout()
+        del nm,p
+
+    #NYC 
+    for i in range(N_TRAIN):
+        ### NATURAL SCENE
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        dim = get_video_dim(train_nyc_path)
+        nm = NaturalMovie(memname, train_nyc_path, 60)
+        nm.warmup(20)
+        p = threading.Thread(target=nm.stream).start()
+        sleep(INTERVAL)
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
+        manager.start_stim()
+        manager()
+        sleep(TRAIN_DUR)    #     manager.set_global_fly_pos(0,0,0)
+        manager.stop_stim()
+
+        manager.black_corner_square()
+        manager()
+        sleep(INTERVAL)
         nm.saveout()
         del nm,p
 
 
-    for i in range(10):
-        ### INIT 
+    ##### TEST ####
+    manager.set_global_fly_pos(0,0,-0.5)
+    # WN
+    for i in range(N_TEST):
         manager.black_corner_square()
         manager.set_idle_background(0)
         manager()
 
-        sleep(3)
+        sleep(INTERVAL)
         
-        ### WHITE NOISE
         rwg = random_word.RandomWords()
-
         memname = rwg.get_random_word()
-        wn = WhiteNoise(memname, (20,60), 20)
+        wn = WhiteNoise(memname, (WN_NPIX,int(800/1920*WN_NPIX)), 20, seed=37)
         p = threading.Thread(target=wn.stream).start()
         manager.start_corner_square()
-        manager.load_stim(name='PixMap', memname=memname, frame_size=(20,60,3))
+        manager.load_stim(name='PixMap', memname=memname, frame_size=(int(800/1920*WN_NPIX),WN_NPIX,3))
         manager.start_stim()
         manager()
 
-        sleep(20)
+        sleep(TEST_DUR)
         
         manager.stop_stim()
         manager.black_corner_square()
         manager.set_idle_background(0)
         manager()
 
-        sleep(2)
+        sleep(INTERVAL)
 
         del wn,p
+    
+    # BAKER
+    for i in range(N_TEST):
+        ### NATURAL SCENE
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        dim = get_video_dim(test_baker_path)
+        nm = NaturalMovie(memname, test_baker_path, 60)
+        p = threading.Thread(target=nm.stream).start()
+        sleep(INTERVAL)
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
+        manager.start_stim()
+        manager()
+        sleep(TEST_DUR)    #     manager.set_global_fly_pos(0,0,0)
+        manager.stop_stim()
+        manager.set_idle_background(0)
+        manager.black_corner_square()
+        manager()
+        sleep(INTERVAL)
+        nm.saveout()
+        del nm,p
+
+    # NYC  
+    for i in range(N_TEST):
+        ### NATURAL SCENE
+        rwg = random_word.RandomWords()
+        memname = rwg.get_random_word()
+        dim = get_video_dim(test_nyc_path)
+        nm = NaturalMovie(memname, test_nyc_path, 60)
+        nm.warmup(20)
+        p = threading.Thread(target=nm.stream).start()
+        sleep(INTERVAL)
+        manager.start_corner_square()
+        manager.load_stim(name='PixMap', memname=memname, frame_size=dim)
+        manager.start_stim()
+        manager()
+        sleep(TEST_DUR)    #     manager.set_global_fly_pos(0,0,0)
+        manager.stop_stim()
+
+        manager.black_corner_square()
+        manager()
+        sleep(INTERVAL)
+        nm.saveout()
+        del nm,p
+
+
 if __name__ == '__main__':
     main()
 
