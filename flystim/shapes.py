7a39bd10102d79c99c1a77fa62f7525a7dfc0e28
Minseung <minseung@stanford.edu>
2023-05-29 02:29:56 -0700

    shapes GlIcosphere

--- flystim/shapes.py
+++ flystim/shapes.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import matlib
 from math import radians
 from .util import rotx, roty, rotz, translate, scale, rotate, rot1_scale_rot2, spherical_to_cartesian, cylindrical_to_cartesian, cylindrical_w_phi_to_cartesian
-
+import icosphere
 
 class GlVertices:
     def __init__(self, vertices=None, colors=None, tex_coords=None):
@@ -382,6 +382,27 @@ class GlPointCollection(GlVertices):
 
         super().__init__(vertices=vertices, colors=colors)
 
+class GlIcosphere(GlVertices):
+    def __init__(self, colors=(1, 1, 1, 1), n_subdivisions=6):
+        '''
+        :param colors: list of colors, one for each face or a single color for all faces; None for default colors
+        :param n_subdivisions: number of subdivisions of each edge of the icosahedron. 
+                                nr_vertex = 12 + 10 * (nu**2 -1) (e.g. 1=12, 2=42, 3=92, 4=162, 5=252, 6=362)
+                                nr_face = 20 * nu**2 (e.g. 1 = 20, 2 = 80, 3 = 180, 4 = 320, 5 = 500, 6 = 720)
+        '''
+        super().__init__()
+
+        vertices, faces = icosphere.icosphere(n_subdivisions)
+        
+        if colors is None:
+            colors = np.linspace(0, 1, len(faces))
+        elif isinstance(colors, tuple):
+            colors = [colors] * len(faces)
+        else:
+            assert len(colors) == len(faces), 'Number of colors must match number of faces'
+        
+        for face, color in zip(faces, colors):
+            self.add(GlTri(vertices[face[0]], vertices[face[1]], vertices[face[2]], getColorList(color)))
 
 class GlCylinder(GlVertices):
     def __init__(self,
